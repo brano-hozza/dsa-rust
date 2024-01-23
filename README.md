ed5967ea8a8e641b74b321d487f43e8081f3a13c
Branislav Hozza <brankohozza@gmail.com>
2024-01-23 19:56:29 +0100

    :memo: add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# DSA Rust
+Learning DSA basics with rust
+
+## Day 1
+- Implement [linked list](./src/list.rs)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use dsa_rust::List;
-
 fn main() {
-    let mut list = List::new();
-    list.push(1);
-    println!("list: {:?}", list);
-    println!("list.peek(): {:?}", list.peek());
-    let poped = list.pop();
-    println!("list.pop(): {:?}", poped);
-    println!("list: {:?}", list);
-    
+    println!("Hello, world!");
 }
